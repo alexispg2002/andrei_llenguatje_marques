ed9e045c7bf1da983f4841368d3164b937e0e123
alexispg2002 <127477580+alexispg2002@users.noreply.github.com>
2023-03-10 08:35:58 +0100

    Add files via upload

---
+++ arbol.js
@@ -0,0 +1,14 @@
+var balls=jQuery('.balls img');
+var velocidad=jQuery('#velocidad');
+var variacion=jQuery('#variacion');
+var start=jQuery('#start');
+
+function var1( ){
+    balls.css("animation-duraion")
+}
+function var2( ){
+    
+}
+function var3( ){
+
+}
